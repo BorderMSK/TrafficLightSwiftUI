ae34a3aaecfbdc5560ec80cd27995b229b6506d4
Igor Makeev <makeevigor@mail.ru>
2021-07-27 23:56:29 +0300

    Initial Commit

---
+++ TrafficLightSwiftUIv2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TrafficLightSwiftUIv2
+//
+//  Created by Igor Makeev on 27.07.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TrafficLightSwiftUIv2/TrafficLightSwiftUIv2App.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightSwiftUIv2App.swift
+//  TrafficLightSwiftUIv2
+//
+//  Created by Igor Makeev on 27.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightSwiftUIv2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
